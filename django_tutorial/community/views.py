90d231cecabf1640f63309a2c694ac16ba11065a
junho <zk836566@gmail.com>
2022-03-16 13:54:41 +0900

    .

--- django_tutorial/community/views.py
+++ django_tutorial/community/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from community.forms import Form
+from .models import Article
 # Create your views here.
 
 # 저장 소스코드
@@ -8,7 +9,17 @@ def write(request):
         form = Form(request.POST)
         if form.is_valid():
             form.save() # 필드값 저장함.
-
+    # 빈 form페이지 요청
     else:
         form =Form()
     return render(request, 'write.html', {'form':form})
+
+    # 글 작성 목록 보여주기
+def articleList(request):
+    article_list = Article.objects.all()
+    return render(request, 'list.html', {'article_list': article_list})
+    
+def viewDetail(request, num=1):
+    article_detail = Article.objects.get(id=num)
+    # article_detail = get_object_or_404(Article, id=num)
+    return render(request, 'view_detail.html', {'article_detail': article_detail})
--- django_tutorial/django_tutorial/urls.py
+++ django_tutorial/django_tutorial/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from community.views import write
+from community.views import viewDetail, write, articleList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('write/', write, name = 'write'), #path, view의 함수
+    path('list/', articleList, name="list"),
+    # /view_detail/1/
+    path('view_detail/<int:num>/', viewDetail, name='view_detail')
 ]
